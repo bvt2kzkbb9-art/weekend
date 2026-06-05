1d6b7bef6893231dcf7a8761d1675ca0cfe7ff51
bvt2kzkbb9-art <bvt2kzkbb9@privaterelay.appleid.com>
2026-06-05 16:47:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const mockData={
+users:[{name:'DJ Nova',artistLevel:12,artistXP:340}],
+posts:[{type:'Set',title:'Techno Night'},{type:'Playlista',title:'Festival Vibes'}],
+challenges:[{name:'Techno Challenge',xp:200}],
+achievements:[{name:'Pierwszy Set'}],
+battleArena:{a:'Set A',b:'Set B',votesA:62,votesB:38}
+};
+console.log('Weekend Music Social',mockData);
